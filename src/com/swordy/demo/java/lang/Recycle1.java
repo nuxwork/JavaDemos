12d2097c0d3360f9372c3c79ebe517d32b754472
swordy <mryangjian@live.com>
2014-02-17 01:49:24 +0800

    1. add： 不建议直接调用finalize();方法，这会导致finalize()执行多次，不便于维护，如果非要这么做，可以定义自己的释放资源的方法（如release(方法)）。

--- src/com/swordy/demo/java/lang/Recycle1.java
+++ src/com/swordy/demo/java/lang/Recycle1.java
@@ -7,16 +7,19 @@ package com.swordy.demo.java.lang;
  * 的这块“特殊”内存。为了应对这种情况，Java允许在类中定义一个名为finalize()的
  * 方法。它的工作原理是一旦垃圾回收器准备好释放对象占用的存储空间，将首先调用其
  * finalize()方法，并且在下一次垃圾回收动作发生时才会真正回收对象占用的内存。
- * 3. 参考android.graphics.Bitmap
+ * 参考android.graphics.Bitmap
+ * 3. 不建议直接调用finalize();方法，这会导致finalize()执行多次，不便于维护，
+ * 如果非要这么做，可以定义自己的释放资源的方法（如release(方法)）。
  * 
  * @author swordy
  *
  */
 public class Recycle1 {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws Throwable {
 
 		System.out.println("sdf");
 		Image img = new Image("d:/image/img1.jpg");
+		img.finalize();
 		img = null;
 		
 		System.gc();
